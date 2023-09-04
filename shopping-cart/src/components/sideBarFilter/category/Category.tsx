da9f8539ce76285a0964f9fc83e960b849c1a3ab
Shagun Dubey <1202shagun@gmail.com>
2023-09-04 23:40:56 +0530

    category

--- shopping-cart/src/components/sideBarFilter/category/Category.tsx
+++ shopping-cart/src/components/sideBarFilter/category/Category.tsx
@@ -1,5 +1,12 @@
 const Category = () => {
-  return <div>Category</div>;
+  return <div>
+    <h2>Category</h2>
+      <p>Shoes</p>
+      <p>Shirts</p>
+      <p>Jeans</p>
+      <p>Shorts</p>
+      <p>Polos</p>
+  </div>;
 };
 
 export default Category;
